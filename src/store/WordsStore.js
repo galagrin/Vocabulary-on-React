5614b86ff269a40e691bf33984342250cfb2cb65
Galina Bobrik <galagrinko@gmail.com>
2024-12-21 17:25:43 +0400

    Fix: change API URLs to HTTPS for production

--- src/store/WordsStore.js
+++ src/store/WordsStore.js
@@ -1,5 +1,5 @@
-import { makeAutoObservable, runInAction } from 'mobx';
-import backUp from '../backUp.json';
+import { makeAutoObservable, runInAction } from "mobx";
+import backUp from "../backUp.json";
 
 class WordsStore {
     dictionary = [];
@@ -17,12 +17,12 @@ class WordsStore {
         }
 
         this.isLoading = true;
-        fetch('http://itgirlschool.justmakeit.ru/api/words')
+        fetch("https://itgirlschool.justmakeit.ru/api/words")
             .then((response) => {
                 if (response.ok) {
                     return response.json();
                 } else {
-                    throw new Error('Something went wrong ...');
+                    throw new Error("Something went wrong ...");
                 }
             })
             .then((data) => {
@@ -32,7 +32,7 @@ class WordsStore {
                 });
             })
             .catch((error) => {
-                console.error('Error fetching words:', error);
+                console.error("Error fetching words:", error);
                 runInAction(() => {
                     this.dictionary = backUp;
                 });
@@ -50,20 +50,20 @@ class WordsStore {
     };
 
     addNewWord = (newWord) => {
-        fetch('http://itgirlschool.justmakeit.ru/api/words/add', {
-            mode: 'no-cors',
-            method: 'POST',
+        fetch("https://itgirlschool.justmakeit.ru/api/words/add", {
+            mode: "no-cors",
+            method: "POST",
             body: JSON.stringify(newWord),
             headers: {
-                'Content-type': 'application/json; charset=UTF-8',
+                "Content-type": "application/json; charset=UTF-8",
             },
         })
             .then((response) => {
                 if (response.ok) {
                     return response.json();
                 } else {
-                    console.log('Response Status:', response.status);
-                    throw new Error('Ошибка добавления нового слова');
+                    console.log("Response Status:", response.status);
+                    throw new Error("Ошибка добавления нового слова");
                 }
             })
             .then((addedWord) => {
@@ -72,53 +72,62 @@ class WordsStore {
                 });
             })
             .catch((error) => {
-                console.error('Ошибка:', error.message);
+                console.error("Ошибка:", error.message);
             });
     };
 
     deleteWord = (id) => {
-        fetch(`http://itgirlschool.justmakeit.ru/api/words/${id}/delete`, {
-            method: 'POST',
+        fetch(`https://itgirlschool.justmakeit.ru/api/words/${id}/delete`, {
+            method: "POST",
         })
             .then((response) => {
                 if (response.ok) {
                     runInAction(() => {
-                        this.dictionary = this.dictionary.filter((item) => item.id !== id);
+                        this.dictionary = this.dictionary.filter(
+                            (item) => item.id !== id
+                        );
                     });
                 } else {
-                    throw new Error('Ошибка удаления слова');
+                    throw new Error("Ошибка удаления слова");
                 }
             })
 
             .catch((error) => {
-                console.error('Ошибка удаления слова', error);
+                console.error("Ошибка удаления слова", error);
             });
     };
 
     updateWord = (updatedWord) => {
-        fetch(`http://itgirlschool.justmakeit.ru/api/words/${updatedWord.id}/update`, {
-            method: 'POST',
-            body: JSON.stringify(updatedWord),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        })
+        fetch(
+            `https://itgirlschool.justmakeit.ru/api/words/${updatedWord.id}/update`,
+            {
+                method: "POST",
+                body: JSON.stringify(updatedWord),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                },
+            }
+        )
             .then((response) => {
                 if (response.ok) {
                     console.log(response.json());
                     return response.json();
                 } else {
-                    console.log('Response Status:', response.status);
-                    throw new Error('Ошибка редактирования слова');
+                    console.log("Response Status:", response.status);
+                    throw new Error("Ошибка редактирования слова");
                 }
             })
             .then((updatedWordResponse) => {
                 runInAction(() => {
-                    this.dictionary = this.dictionary.map((word) => (word.id === updatedWordResponse.id ? updatedWordResponse : word));
+                    this.dictionary = this.dictionary.map((word) =>
+                        word.id === updatedWordResponse.id
+                            ? updatedWordResponse
+                            : word
+                    );
                 });
             })
             .catch((error) => {
-                console.error('Ошибка:', error.message);
+                console.error("Ошибка:", error.message);
             });
     };
 }
